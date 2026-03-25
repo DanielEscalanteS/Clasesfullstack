99b29b2a95e6122dd8c8950f9c688ec9d0a34bf2
Daniel <tu@email.com>
2026-03-25 13:34:32 -0300

    repository

--- src/main/java/com/example/bibliotecaduoc/Repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/Repository/LibroRepository.java
@@ -1,5 +1,59 @@
 package com.example.bibliotecaduoc.Repository;
 
+
+import com.example.bibliotecaduoc.Model.libro;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
 public class LibroRepository {
+    private List<libro> listaLibros = new ArrayList<>();
+
+    public List<libro> obteneLibros(){
+
+        return listaLibros;
+    }
+
+    public libro buscarPorId(int id){
+        for (libro libro : listaLibros){
+
+            if (libro.getId()==id){
+                return libro;
+            }
+        }
+        return null;
+
+    }
+
+    public libro buscarPorIsbn(String isbn){
+        for (libro libro : listaLibros){
+
+            if (libro.getIsbn().equals(isbn)){
+                return libro;
+            }
+        }
+        return null;
+
+    }
+
+    public libro guardar(Libro lib){
+        listaLibros.add(lib);
+        return lib;
+
+    }
+
+    public libro actualizar(libro lib){
+
+        int id  =0;
+        int idposition = 0;
+
+        return null;
+
+    }
+  
+
     
 }
